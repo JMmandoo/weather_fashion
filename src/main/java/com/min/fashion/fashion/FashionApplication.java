23d0ef8997a863d616af96bff9be2f418813d4e1
JMmandoo <leejeamon1994@gmail.com>
2023-10-20 00:02:17 +0900

    Feat: 로그인, 회원가입 기능 추가
    - 홈에서 회원가입 및 로그인 구현

--- src/main/java/com/min/fashion/fashion/FashionApplication.java
+++ src/main/java/com/min/fashion/fashion/FashionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FashionApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FashionApplication.class, args);
 	}
 
---
+++ src/main/java/com/min/fashion/fashion/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.min.fashion.fashion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+  @GetMapping("/")
+  public String index(){
+    return "index";
+  }
+}
