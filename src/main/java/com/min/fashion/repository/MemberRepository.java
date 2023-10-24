e781de15ebeea0f95a825e4c4a828b1f0c1cd11f
ljmin <dfty1610@naver.com>
2023-10-24 18:17:26 +0900

    Feat: 회원정보 수정, 메인화면 백그라운드 동영상 삽입 - 메인화면 날씨별 영상소스 넣음 - 회원정보 수정, 로그아웃 기능 구현

--- src/main/java/com/min/fashion/repository/MemberRepository.java
+++ src/main/java/com/min/fashion/repository/MemberRepository.java
@@ -2,9 +2,10 @@ package com.min.fashion.repository;
 
 import com.min.fashion.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-
+    // 이메일로 회원 정보 조회 (select * from member_table where member_email=?)
+    Optional<MemberEntity> findByMemberEmail(String memberEmail);
 }
