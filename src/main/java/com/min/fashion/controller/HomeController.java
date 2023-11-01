b069b17906661d50c924e629c8743185bbbafda7
ljmin <dfty1610@naver.com>
2023-11-01 17:47:33 +0900

    DOCS: index.html 사진추가 - 메인페이지 사진추가

--- src/main/java/com/min/fashion/controller/HomeController.java
+++ src/main/java/com/min/fashion/controller/HomeController.java
@@ -11,19 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @AllArgsConstructor
 public class HomeController {
   @GetMapping("/")
-  public String index(){
+  public String index() {
     return "index";
   }
-  @GetMapping("/user") //user만 들어갈수 있음
-  public String user(){
-    return "user";
-  }
-
-  @GetMapping("login/{oauth2")
-  public String loginGoogle(@PathVariable String oauth2, HttpServletResponse httpServletResponse){
-    return  "redirect:/oauth2/authorization/" + oauth2;
-  }
-  //접근권한 없을때
-  @RequestMapping("/accessDenied")
-  public String accessDenied(){return "accessDenied";}
 }
--- src/main/java/com/min/fashion/dto/Role.java
+++
@@ -1,5 +0,0 @@
-package com.min.fashion.dto;
-
-public enum Role {
-  ROLE_USER;
-}
